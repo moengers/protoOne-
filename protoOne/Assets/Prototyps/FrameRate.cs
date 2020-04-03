86d3f1e1866025dd6e64c3f9170b9d57ae20bbc9
Momo <hohenheim@mailbox.org>
2020-04-03 20:59:29 +0200

    Some Testing

---
+++ protoOne/Assets/Prototyps/FrameRate.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FrameRate : MonoBehaviour
+{
+
+    [SerializeField] private int TargetFrameRate = 60;
+    
+    void Awake()
+    {
+        QualitySettings.vSyncCount = 0;
+        Application.targetFrameRate = TargetFrameRate;
+    }
+    void Update()
+    {
+        if(Application.targetFrameRate != TargetFrameRate)
+            Application.targetFrameRate = TargetFrameRate;
+    }
+}
